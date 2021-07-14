ced07f29d7fd456fe07daa0add3dfe0f2bbd1b45
Sergey Litvinov <slitvinov@gmail.com>
2021-07-14 16:30:02 +0200

    add Pendulum.py

---
+++ Pendulum.py
@@ -0,0 +1,19 @@
+import gym
+import util
+
+env = gym.make("Pendulum-v0")
+obs = env.reset()
+R = 0
+while True:
+    util.render(env)
+    if obs[0] > 0:
+        action = -9*obs[1] - 2*obs[2]
+    else:
+        action = obs[2]/5
+    obs, reward, done, info = env.step([action])
+    R += reward
+    if done:
+        util.render(env)
+        print(R)
+        break
+
