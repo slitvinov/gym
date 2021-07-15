9a48b2b18e95bf04b4e4b7374f4d1d8f0012fd5d
Sergey Litvinov <slitvinov@gmail.com>
2021-07-15 11:06:22 +0200

    minor

--- MountainCar.py
+++ MountainCar.py
@@ -5,7 +5,7 @@ obs = env.reset()
 R = 0
 while True:
     util.render(env)
-    action = 0 if obs[1] < 0 else 2
+    action = 2 if obs[1] > 0 else 0
     obs, reward, done, info = env.step(action)
     R += reward
     if done:
