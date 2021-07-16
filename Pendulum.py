402478d5e6df943e118dde51c0e7f6242a4de3b4
Sergey Litvinov <slitvinov@gmail.com>
2021-07-16 16:16:05 +0200

    one liner

--- Pendulum.py
+++ Pendulum.py
@@ -6,10 +6,7 @@ obs = env.reset()
 R = 0
 while True:
     util.render(env)
-    if obs[0] > 0:
-        action = -9*obs[1] - 2*obs[2]
-    else:
-        action = obs[2]
+    action = -9*obs[1] - 2*obs[2] if obs[0] >= 0 else obs[2]
     obs, reward, done, info = env.step([action])
     R += reward
     if done:
